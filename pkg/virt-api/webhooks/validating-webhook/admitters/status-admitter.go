7808f2d848103e35d441baf7261d2c7f3d79c21c
Howard Gao <howard.gao@gmail.com>
2025-12-10 20:37:37 +0800

    debug

--- pkg/virt-api/webhooks/validating-webhook/admitters/status-admitter.go
+++ pkg/virt-api/webhooks/validating-webhook/admitters/status-admitter.go
@@ -2,6 +2,7 @@ package admitters
 
 import (
 	"context"
+	"fmt"
 
 	admissionv1 "k8s.io/api/admission/v1"
 
@@ -14,14 +15,27 @@ type StatusAdmitter struct {
 	VmsAdmitter *VMsAdmitter
 }
 
+func log(fmtter string, vals ...any) {
+	fmt.Printf("[debug]"+fmtter+"\n", vals...)
+}
+
 func (s *StatusAdmitter) Admit(ctx context.Context, ar *admissionv1.AdmissionReview) *admissionv1.AdmissionResponse {
+
+	log("In StatusAdmitter.Admit, validating status...")
+
 	if resp := webhooks.ValidateStatus(ar.Request.Object.Raw); resp != nil {
+		log("something returned from ValidateStatus...")
 		return resp
 	}
 
 	if webhooks.ValidateRequestResource(ar.Request.Resource, webhooks2.VirtualMachineGroupVersionResource.Group, webhooks2.VirtualMachineGroupVersionResource.Resource) {
-		return s.VmsAdmitter.AdmitStatus(ctx, ar)
+		log("need validate resource...")
+		result := s.VmsAdmitter.AdmitStatus(ctx, ar)
+		log("validate resource result %v", result)
+		return result
 	}
+
+	log("all good the request is allowed")
 
 	reviewResponse := admissionv1.AdmissionResponse{}
 	reviewResponse.Allowed = true
